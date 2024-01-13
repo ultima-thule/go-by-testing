5e380ea9b54e0f6a5ca258d4a4c107d25d82d9f6
Joanna Grzywna <jgrzywna@splunk.com>
2024-01-13 21:44:49 +0100

    Chapter: Arrays and Slices

--- arrslices/sum.go
+++ arrslices/sum.go
@@ -9,3 +9,28 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numsToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numsToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numsToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numsToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
