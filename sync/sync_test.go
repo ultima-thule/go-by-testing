a18288560bab058c7d242ef0d4c9e74bff964915
ultima-thule <joanna.grzywna@gmail.com>
2024-01-14 21:56:48 +0100

    Chapter: Sync

---
+++ sync/sync_test.go
@@ -0,0 +1,43 @@
+package synctest
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing counter 3x leaves it at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	if got.Value() != want {
+		t.Errorf("got %d want %d", got.Value(), want)
+	}
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
