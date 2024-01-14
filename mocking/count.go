2bb9b2b466406f9254dfb8c57d4c4c6d7276d5b3
ultima-thule <joanna.grzywna@gmail.com>
2024-01-14 11:41:07 +0100

    Chapter: Mocking

---
+++ mocking/count.go
@@ -0,0 +1,37 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const (
+	countdownStart = 3
+	finalWord      = "Go!"
+
+	sleep = "sleep"
+	write = "write"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
