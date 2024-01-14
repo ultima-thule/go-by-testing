06ed886d02b9e5694dbcdb093efb467fc761e110
ultima-thule <joanna.grzywna@gmail.com>
2024-01-14 10:40:19 +0100

    Chapter: DInj

---
+++ depinj/di.go
@@ -0,0 +1,21 @@
+package di
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
+	// Greet(os.Stdout, "Elodie")
+}
